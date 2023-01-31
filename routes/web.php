9ecd7d4a180e074a9fb8cf5e511a411e3ffbcb5f
keisuke81 <n.kei.0801.1997@gmail.com>
2023-01-31 20:30:49 +0900

    [update]routeの改善
    ログイン状態でないとアクセスできないページの精査と対応

--- routes/web.php
+++ routes/web.php
@@ -37,11 +37,11 @@ Route::middleware('auth')->group(function () {
 Route::controller(HotaruRequestController::class)->group(
     function () {
         Route::get('/request','getRequest')->name('request.index');
-        Route::post('/request/session', 'sessionSave')->name('request.session.save');
-        Route::get('/request/ohakamairi/confirm', 'ohakamairiConfirm')->name('ohakamairi.confirm');
-        Route::get('/request/omamori/confirm', 'omamoriConfirm')->name('omamori.confirm');
-        Route::get('/request/sanpai/confirm', 'sanpaiConfirm')->name('sanpai.confirm');
-        Route::get('/request/others/confirm', 'othersConfirm')->name('others.confirm');
+        Route::post('/request/session', 'sessionSave')->name('request.session.save')->middleware('auth');
+        Route::get('/request/ohakamairi/confirm', 'ohakamairiConfirm')->name('ohakamairi.confirm')->middleware('auth');
+        Route::get('/request/omamori/confirm', 'omamoriConfirm')->name('omamori.confirm')->middleware('auth');
+        Route::get('/request/sanpai/confirm', 'sanpaiConfirm')->name('sanpai.confirm')->middleware('auth');
+        Route::get('/request/others/confirm', 'othersConfirm')->name('others.confirm')->middleware('auth');
 
         //依頼完了
         Route::post('/request/done', 'done')->name('request.done');
@@ -53,46 +53,46 @@ Route::controller(HotaruRequestController::class)->group(
 
        
 
-        Route::get('/chat', 'getChat')->name('chat.index');
+        Route::get('/chat', 'getChat')->name('chat.index')->middleware('auth');
 
 
         //////////プランごとに依頼を作成する画面/////////////
-        Route::get('/request/ohakamairi', 'getOhakamairi')->name('request.ohakamairi');
-        Route::get('/request/omamori', 'getOmamori')->name('request.omamori');
-        Route::get('/request/sanpai', 'getSanpai')->name('request.sanpai');
-        Route::get('/request/others', 'getOthers')->name('request.others');
-        Route::post('/search/apply', 'searchApply')->name('search.apply');
+        Route::get('/request/ohakamairi', 'getOhakamairi')->name('request.ohakamairi')->middleware('auth');
+        Route::get('/request/omamori', 'getOmamori')->name('request.omamori')->middleware('auth');
+        Route::get('/request/sanpai', 'getSanpai')->name('request.sanpai')->middleware('auth');
+        Route::get('/request/others', 'getOthers')->name('request.others')->middleware('auth');
+        Route::post('/search/apply', 'searchApply')->name('search.apply')->middleware('auth');
 
     });
 
 Route::controller(UserProfileController::class)->group(
     function () {
         Route::get('/mypage', 'getMypage')->name('mypage.index');
-        Route::get('/mypage/myprofile', 'getMyProfile')->name('myprofile.index');
-        Route::get('/mypage/myprofile/edit', 'editMyProfile')->name('myprofile.edit');
-        Route::post('/mypage/myprofile/update', 'updateMyProfile')->name('myprofile.update');
-        Route::post('/mypage/myprofile/image_update', 'updateImage')->name('myprofile.image.update');
+        Route::get('/mypage/myprofile', 'getMyProfile')->name('myprofile.index')->middleware('auth');
+        Route::get('/mypage/myprofile/edit', 'editMyProfile')->name('myprofile.edit')->middleware('auth');
+        Route::post('/mypage/myprofile/update', 'updateMyProfile')->name('myprofile.update')->middleware('auth');
+        Route::post('/mypage/myprofile/image_update', 'updateImage')->name('myprofile.image.update')->middleware('auth');
 
-        Route::get('/mypage/request', 'getMyRequest')->name('mypage.myrequest.index');
-        Route::get('/mypage/myrequest/detail/{request_id}', 'getMyRequestDetail')->name('mypage.myrequest.detail');
-        Route::get('/mypage/myrequest/edit/{request_id}', 'editMyRequest')->name('mypage.myrequest.edit');
-        Route::get('/mypage/myrequest/destroy/{request_id}', 'destroyMyRequest')->name('mypage.myrequest.destroy');
-        Route::get('/mypage/myrequest/member_list/{request_id}', 'getApplyMemberList')->name('mypage.myrequest.member_list');
-        Route::get('/mypage/myrequest/member_detail/{request_id}/{user_id}/{apply_id}', 'getApplyMemberDetail')->name('mypage.myrequest.member_detail');
-        Route::get('/mypage/myrequest/member_detail/approval/{apply_id}/{request_id}/{user_id}', 'getApplyApproval')->name('myrequest.member.approval');
-        Route::get('/mypage/myrequest/member_detail/reject/{apply_id}/{request_id}/{user_id}', 'getApplyReject')->name('myrequest.member.reject');
+        Route::get('/mypage/request', 'getMyRequest')->name('mypage.myrequest.index')->middleware('auth');
+        Route::get('/mypage/myrequest/detail/{request_id}', 'getMyRequestDetail')->name('mypage.myrequest.detail')->middleware('auth');
+        Route::get('/mypage/myrequest/edit/{request_id}', 'editMyRequest')->name('mypage.myrequest.edit')->middleware('auth');
+        Route::get('/mypage/myrequest/destroy/{request_id}', 'destroyMyRequest')->name('mypage.myrequest.destroy')->middleware('auth');
+        Route::get('/mypage/myrequest/member_list/{request_id}', 'getApplyMemberList')->name('mypage.myrequest.member_list')->middleware('auth');
+        Route::get('/mypage/myrequest/member_detail/{request_id}/{user_id}/{apply_id}', 'getApplyMemberDetail')->name('mypage.myrequest.member_detail')->middleware('auth');
+        Route::get('/mypage/myrequest/member_detail/approval/{apply_id}/{request_id}/{user_id}', 'getApplyApproval')->name('myrequest.member.approval')->middleware('auth');
+        Route::get('/mypage/myrequest/member_detail/reject/{apply_id}/{request_id}/{user_id}', 'getApplyReject')->name('myrequest.member.reject')->middleware('auth');
 
 
-        Route::get('/mypage/myapply', 'getMyApply')->name('mypage.myapply.index');
-        Route::get('/mypage/myapply/detail/{request_id}', 'getMyApplyDetail')->name('mypage.myapply.detail');
+        Route::get('/mypage/myapply', 'getMyApply')->name('mypage.myapply.index')->middleware('auth');
+        Route::get('/mypage/myapply/detail/{request_id}', 'getMyApplyDetail')->name('mypage.myapply.detail')->middleware('auth');
 
     });
 
 Route::controller(ChatController::class)->group(
     function (){
-        Route::get('/chat/list','getChatList')->name('chat.list');
-        Route::get('/chat/room/{apply_id}/{your_id}', 'getChatRoom')->name('chat.room');
-        Route::post('/chat/send', 'sendChat')->name('send.chat');
+        Route::get('/chat/list','getChatList')->name('chat.list')->middleware('auth');
+        Route::get('/chat/room/{apply_id}/{your_id}', 'getChatRoom')->name('chat.room')->middleware('auth');
+        Route::post('/chat/send', 'sendChat')->name('send.chat')->middleware('auth');
 
     });
 
